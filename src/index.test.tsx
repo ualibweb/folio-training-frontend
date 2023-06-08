d978137b84d8064be40e6d9d5227fbba7078c32c
danetsao <danetsaoo@gmail.com>
2023-06-08 12:15:25 -0500

    Added BrowserRouter to index.test.tsx

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { MemoryRouter } from 'react-router';
 import App from './index';
 import withIntlConfiguration from './test/util/withIntlConfiguration';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 describe('Main page', () => {
   it('renders the app', () => {
-    render(withIntlConfiguration(<MemoryRouter><App /></MemoryRouter>));
+    render(withIntlConfiguration(<BrowserRouter><App /></BrowserRouter>));
 
     // Only renders <body><div /></body>, so just testing if it renders
   });
