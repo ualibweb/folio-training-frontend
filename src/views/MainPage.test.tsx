c2301c0fee27ed07693624e89d1d018122161fe9
danetsao <danetsaoo@gmail.com>
2023-06-09 14:55:18 -0500

    Added tests to improve coverage

--- src/views/MainPage.test.tsx
+++ src/views/MainPage.test.tsx
@@ -4,6 +4,7 @@ import MainPage from './MainPage';
 import withIntlConfiguration from '../test/util/withIntlConfiguration';
 
 import useInstitutions from '../hooks/useInstitutions';
+import userEvent from '@testing-library/user-event';
 
 // at top of file, before the tests
 jest.mock('../hooks/useInstitutions');
@@ -32,5 +33,44 @@ describe('MainPage', () => {
     // ensure that the list is rendered
     expect(screen.getByText('institution 1')).toBeInTheDocument();
     expect(screen.getByText('institution 2')).toBeInTheDocument();
+  });
+
+  it('shows the text', async () => {
+    render(withIntlConfiguration(<MainPage />));
+
+    expect(screen.getByText('Show Dismissable Pane')).toBeVisible();
+  });
+
+  it('click the toggle window button', async () => {
+    render(withIntlConfiguration(<MainPage />));
+
+    // See if we can see the dismissable pane before clicking the button
+    expect(screen.queryByText('This is a dismissable pane')).toBeNull();
+
+    const showButton = screen.getByRole('button', { name: 'Show Dismissable Pane' });
+
+    await userEvent.click(showButton);
+
+    // See if we can see the dismissable pane after clicking the button
+    expect(screen.queryByText('This is a dismissable pane')).toBeVisible();
+  });
+
+  it('click the toggle window button, then close it', async () => {
+    render(withIntlConfiguration(<MainPage />));
+
+    // See if we can see the dismissable pane before clicking the button
+    const showButton = screen.getByRole('button', { name: 'Show Dismissable Pane' });
+
+    await userEvent.click(showButton);
+
+    // See if we can see the dismissable pane after clicking the button
+    expect(screen.queryByText('This is a dismissable pane')).toBeVisible();
+
+    const closeButton = screen.getByRole('button', { name: 'Close Pane' });
+
+    await userEvent.click(closeButton);
+
+    // See if we can see the dismissable pane after clicking the button
+    expect(screen.queryByText('This is a dismissable pane')).toBeNull();
   });
 });
