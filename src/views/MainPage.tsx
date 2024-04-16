fd4550290d0c06e6f278e7454e54efe2dac192d0
Chris Hellen <chellen@ua.edu>
2024-04-16 12:27:04 -0500

    Implemented dismissable second panel

--- src/views/MainPage.tsx
+++ src/views/MainPage.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import css from './MainPage.module.css';
+import { Pane, Paneset, PaneHeader, Headline, MultiColumnList, Button } from '@folio/stripes/components';
+import { useState } from 'react';
 
 export default function MainPage() {
+  const [showPanel, setShowPanel] = useState<boolean>(false);
+
+  const openPanel = () => {
+    setShowPanel(true);
+  }
+
+  const closePanel = () => {
+    setShowPanel(false);
+  }
+
+  const bookData = [
+    {"Title": "The Cat in the Hat", "Author": "Dr. Suess"},
+    {"Title": "The Great Gatsby", "Author": "F. Scott Fitzgerald"}
+  ]
+
   return (
-    <div className={css.wrapper}>
-      <h1>Cool stuff is coming</h1>
-      <span className={css.eyesWrapper}>
-        <span className={css.eyes}>👀</span>
-      </span>
+    <div>
+      <Paneset>
+        <Pane defaultWidth="fill">
+          <Headline size='large' margin='medium' tag='h3'>
+            Headline
+            <Button onClick={openPanel}>
+              Open side panel
+            </Button>
+          </Headline>
+          <MultiColumnList contentData={bookData} />
+        </Pane>
+        {showPanel && 
+          <Pane 
+          defaultWidth='50%' 
+          renderHeader={renderProps => (
+            <PaneHeader
+            {...renderProps}
+            dismissible
+            onClose={closePanel}
+            paneTitle="Dismiss this"
+            />
+          )}>
+            Side panel
+          </Pane>
+        }
+      </Paneset>
     </div>
   );
 }
