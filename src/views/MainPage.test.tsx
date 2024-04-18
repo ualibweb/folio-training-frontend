01f494bf3640ac3c7e57b7fa4dfda8832a4f03bc
Chris Hellen <chellen@ua.edu>
2024-04-18 15:00:02 -0500

    Created tests for Debug and SidePanel components. Adapted MainPage tests to changes

--- src/views/MainPage.test.tsx
+++ src/views/MainPage.test.tsx
@@ -1,11 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { getByText, render, screen } from '@testing-library/react';
 import React from 'react';
 import MainPage from './MainPage';
+import userEvent from '@testing-library/user-event'
+import withIntlConfiguration from '../test/util/withIntlConfiguration';
 
 describe('Main page', () => {
-  it('shows the text', async () => {
-    render(<MainPage />);
+  it('shows correct text', async () => {
+    render(withIntlConfiguration(<MainPage />));
 
-    expect(await screen.findByText('Cool stuff is coming')).toBeVisible();
+    expect(screen.getByRole('heading', { name: 'Headline Open side panel' })).toBeVisible();
+
+    await userEvent.click(await screen.findByRole('button'));
+    expect(screen.getByText('Side panel')).toBeVisible();
   });
 });
