da4d10a4be97cec16a8fed7c2b360ed7e20a3197
Chris Hellen <chellen@ua.edu>
2024-04-30 10:17:26 -0500

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,42 +1,49 @@
 const { join } = require('path');
 const config = require('@folio/jest-config-stripes');
 
-/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  ...config,
-
-  coverageProvider: 'v8',
-
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
-
-  testMatch: ['**/src/**/?(*.)test.{js,jsx,ts,tsx}'],
-
-  coverageReporters: ['lcov', 'text'],
-  collectCoverageFrom: [
-    '<rootDir>/index.ts',
-    '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/**/*.d.ts',
-    '!<rootDir>/src/**/*.test.{ts,tsx}',
-    '!<rootDir>/src/test/**',
-    '!**/node_modules/**',
-  ],
-
-  setupFiles: [...config.setupFiles, join(__dirname, './src/test/setupTests.ts')],
-  setupFilesAfterEnv: [join(__dirname, './src/test/jest.setup.ts')],
+  testEnvironment: 'jsdom',
 
   preset: 'ts-jest',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     ...config.transform,
   },
+  transformIgnorePatterns: ['node_modules/(?!@folio|ky)'],
+
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+
+  testMatch: ['**/src/**/?(*.)test.{js,jsx,ts,tsx}'],
+  testPathIgnorePatterns: ['/node_modules/', 'src/typings/'],
+
+  reporters: ['default', 'jest-junit'],
+
+  coverageReporters: ['lcov', 'text'],
+  coverageDirectory: './artifacts/coverage-jest/',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/test/**',
+    '!**/node_modules/**'
+  ],
+
+  setupFiles: [join(__dirname, './src/test/setupTests.ts')],
+  setupFilesAfterEnv: [join(__dirname, './src/test/jest.setup.ts')],
 
   moduleNameMapper: {
     '^.+\\.(css|svg)$': 'identity-obj-proxy',
 
     // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
-    uuid: require.resolve('uuid'),
+    uuid: require.resolve('uuid')
   },
 
   slowTestThreshold: 10,
-  testTimeout: 20000,
+
+  globals: {
+    'ts-jest': {
+      tsconfig: 'src/tsconfig.json'
+    }
+  }
 };
