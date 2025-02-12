b382118e0b3195391df28790ba1a681683cf738f
Kavya Gupta <kgupta1@crimson.ua.edu>
2025-02-11 21:50:29 -0600

    completed frontend 02 UI/UX training

--- src/views/MainPage.tsx
+++ src/views/MainPage.tsx
@@ -1,13 +1,67 @@
 import React from 'react';
-import css from './MainPage.module.css';
+import { Headline, MultiColumnList, Pane, PaneHeader, Paneset, Button, Icon } from '@folio/stripes/components';
+import { useState } from 'react';
 
 export default function MainPage() {
+  
+  const cars = [
+    { name: "Model S", manufacturer: "Tesla", year: 2023, type: "Electric" },
+    { name: "Mustang", manufacturer: "Ford", year: 2022, type: "Sports" },
+    { name: "Civic", manufacturer: "Honda", year: 2021, type: "Sedan" },
+    { name: "Camry", manufacturer: "Toyota", year: 2023, type: "Sedan" },
+    { name: "911", manufacturer: "Porsche", year: 2024, type: "Sports" }
+  ];
+  
+  const [showPanel, setShowPanel] = useState<boolean>(false);
+
+  const open = () => {
+    setShowPanel(true);
+  }
+
+  const close = () => {
+    setShowPanel(false);
+  }
+
   return (
-    <div className={css.wrapper}>
-      <h1>Cool stuff is coming</h1>
-      <span className={css.eyesWrapper}>
-        <span className={css.eyes}>👀</span>
-      </span>
-    </div>
+    <Paneset>
+      <Pane
+        defaultWidth="20%"
+        paneTitle="Filters"
+      >
+        <Headline>
+        <div style={{ textAlign: "right" }}>
+        <div style={{ textAlign: "center" }}>
+            Pane 1
+        </div>
+          <Button onClick={open}>
+            <Icon
+              icon="arrow-right"
+              size="large"
+              iconClassName="myClass"
+            />
+          </Button> 
+        </div>
+        
+       
+        </Headline>
+      
+      <MultiColumnList contentData={cars}/>
+      </Pane>
+      
+       {showPanel && 
+        <Pane 
+        defaultWidth='50%' 
+        renderHeader={renderProps => (
+          <PaneHeader
+          {...renderProps}
+          dismissible
+          onClose={close}
+          paneTitle="Side Pane"
+          />
+        )}>
+          Side pane
+        </Pane>
+      }
+    </Paneset>
   );
 }
