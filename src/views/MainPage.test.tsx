5aa9837f08a649820d691787624710d41aa22e3d
sushimpaudel264-alt <spaudel4@crimson.ua.edu>
2026-07-27 16:03:19 -0500

    Add basic layout with dynamic details pane

--- src/views/MainPage.test.tsx
+++ src/views/MainPage.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from '@folio/jest-config-stripes/testing-library/react';
 import React from 'react';
 import MainPage from './MainPage';
+import {Button} from '@folio/stripes/components';
 
 describe('Main page', () => {
   it('shows the text', async () => {
     render(<MainPage />);
 
-    expect(await screen.findByText('Cool stuff is coming')).toBeVisible();
+    expect(await screen.findByText('Hyalo')).toBeVisible();
   });
 });
